6826abf342980fb709c81a6aa5b98df31a33cfd3
yadea <kuanyiaun@hotmail.com>
2015-10-26 17:05:31 +0800

    Added Drawer Layout Activity.

--- app/src/main/java/com/yadea/budgetplanner/MainActivity.java
+++ app/src/main/java/com/yadea/budgetplanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yadea.budgetplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, SideMenuActivity.class);
+        this.startActivity(intent);
     }
 
 
