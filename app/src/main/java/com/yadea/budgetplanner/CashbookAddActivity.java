9d65e15b082632892a4c07fbbd31cc05646cd45e
yadea <kuanyiaun@hotmail.com>
2015-10-27 17:18:33 +0800

    Changed UI to TableLayout. Added close functions and open functions to emulate saving and save and new entry in Cashbook Add.

--- app/src/main/java/com/yadea/budgetplanner/CashbookAddActivity.java
+++ app/src/main/java/com/yadea/budgetplanner/CashbookAddActivity.java
@@ -1,10 +1,12 @@
 package com.yadea.budgetplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class CashbookAddActivity extends AppCompatActivity {
 
@@ -23,5 +25,29 @@ public class CashbookAddActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.activity_cashbook_add_menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_save) {
+            //// TODO: 27/10/2015 add save feature
+            finish();
+            return true;
+        }
+        else if (id == R.id.action_save_add) {
+            // // TODO: 27/10/2015 add save feature
+            Intent intent = new Intent(this,CashbookAddActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
